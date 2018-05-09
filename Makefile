73df419ca35311bcb7320a2d54f943a0da649646
toidiu <apoorv@toidiu.com>
2018-05-08 21:42:18 -0400

    starting client anew.. update make file with node proto generator

--- Makefile
+++ Makefile
@@ -20,4 +20,7 @@ get:
 server:
 	cargo run --bin server
 
+jsproto:
+	protoc --proto_path=proto --js_out=binary:client/src  proto/proto/dkv/dkv.proto
+
 .PHONY: createLocalS3 createLocalDropbox deleteLocalDropbox deleteLocalS3 get set server
--- client/generate_proto.sh
+++
@@ -1,5 +0,0 @@
- # protoc --proto_path=../proto --js_out=import_style=commonjs,binary:js ../proto/proto/dkv/dkv.proto
-
-
- grpc_tools_node_protoc --proto_path=../proto --js_out=import_style=commonjs,binary:js/ --grpc_out=js --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` ../proto/proto/dkv/dkv.proto
-
