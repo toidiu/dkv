47ba98e490f21e23fab9ac3ed1597ec8f057fa4e
toidiu <apoorv@toidiu.com>
2018-06-28 23:59:36 -0400

    .

--- client/index.js
+++ client/index.js
@@ -1,1 +1,33 @@
-console.log("asdf");
+var messages = require('./proto/dkv/dkv_pb');
+var services = require('./proto/dkv/dkv_grpc_pb');
+
+
+var grpc = require('grpc');
+
+function main() {
+
+  console.log("run");
+    var client = new services.DkvClient('localhost:50051',
+                   grpc.credentials.createInsecure());
+
+    var request = new messages.GetKeyRequest();
+    request.setKey("key");
+    // console.log(request);
+
+    var request1 = new messages.AddKeyRequest();
+    request1.setKey("bla");
+    request1.setData("bla data");
+    console.log(request1.getData());
+
+
+    client.getKey(request, function(err, resp){
+      console.log(resp)
+    });
+
+    client.addKey(request1, function(err, resp){
+      console.log(resp)
+    });
+
+}
+
+main();
